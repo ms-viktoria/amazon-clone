d82ed0f0fd6fb4634d25b723dbf072ee9cbaf414
ms-viktoria <melnikviktorialeo@gmail.com>
2024-11-05 20:58:16 +0200

    JavaScript tutorial - Day-13

--- amazon-project/data/cart.js
+++ amazon-project/data/cart.js
@@ -1,16 +1,13 @@
 export let cart = JSON.parse(localStorage.getItem('cart'));
 
-
-if(!cart) {
+if (!cart) {
   cart = [{
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2,
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1,
-  },
-];
+  }, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+  }];
 }
 
 function saveToStorage() {
@@ -31,7 +28,7 @@ export function addToCart(productId) {
   } else {
     cart.push({
       productId: productId,
-      quantity: 1,
+      quantity: 1
     });
   }
 
@@ -48,6 +45,28 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+
+  saveToStorage();
+}
+
+export function calculateQuantity() {
+  let cartQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity
+  });
+  return cartQuantity;
+}
+
+export function updateQuantity(productId, newQuantity) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+  matchingItem.quantity = newQuantity;
 
   saveToStorage();
 }
